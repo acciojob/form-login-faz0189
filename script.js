976fffa9c65ddcf784598f6b468604c2a0b82448
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-14 18:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function getFormvalue(event) {
-            event.preventDefault();
-            const fname = document.forms[0].fname.value;
-            const lname = document.forms[0].lname.value;
-            alert(fname + ' ' + lname);
-        }
+function getFormvalue() {
+    // Prevent form submission
+    event.preventDefault();
+
+    // Get form elements
+    const form = document.getElementById('form1');
+
+    // Retrieve values
+    const firstName = form.fname.value;
+    const lastName = form.lname.value;
+
+    // Display full name
+    alert(firstName + ' ' + lastName);
+}
